e031107b377ad4d1edc6d8b5eae5b1a5f6a06236
Hamed Tijane Mkpoumie Monfouayi <79936289+mkp21-kamer@users.noreply.github.com>
2025-07-29 23:53:55 +0000

    Mise à jour du fichier App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
+  name: "Tijane Mkpoumie",
+  title: "Web Developer",
+  email: "tijanemonfouayi@gmail.com",
+  gitHub: "https://github.com/mkp21-kamer",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
+  linkedIn: "https://www.linkedin.com/in/tijanemonfouayi/",
   medium: "",
   twitter: "microsoft",
   youTube: "Code",
